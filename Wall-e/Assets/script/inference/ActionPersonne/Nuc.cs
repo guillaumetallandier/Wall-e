b4012829f070cdd3d30a4a683af2b395e85dee2b
Benjamin <benjamin.marchal@etu.unitra.fr>
2019-12-12 13:49:15 +0100

    Scenario nucleaire

---
+++ Wall-e/Assets/script/inference/ActionPersonne/Nuc.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nuc : Action
+{
+
+    public override void execute(GameObject go, GameObject gm)
+    {
+        GameObject.FindGameObjectWithTag("maitre").GetComponent<Master>().RecupTexte("NucDebut", gm.GetComponent<Personne>().name, "");
+        gm.GetComponent<Observable>().notifyObservers(this.getType(), gm.gameObject);
+
+    }
+    public override string getType()
+    {
+        return "Nucleaire";
+    }
+}
--- Wall-e/Assets/script/inference/robotAction/Tuer.cs
+++ Wall-e/Assets/script/inference/robotAction/Tuer.cs
@@ -6,7 +6,13 @@ public class Tuer : Action
 {
     override public void execute(GameObject gop, GameObject gm)
     {
-        GameObject.FindGameObjectWithTag("maitre").GetComponent<Master>().RecupTexte("tuer", "", gm.name);
+        string action=gm.GetComponent<Personne>().getAction().getType();
+
+        if(action== "Nucleaire")
+        {
+            GameObject.FindGameObjectWithTag("maitre").GetComponent<Master>().RecupTexte("tuer", "","");
+        }
+        //GameObject.FindGameObjectWithTag("maitre").GetComponent<Master>().RecupTexte("tuer", "", gm.name);
         gm.GetComponent<Personne>().die();
     }
 
